f985787b577e3e52c2d811104f604a3d5f8cafb1
notVinh <dqv4work@gmail.com>
2025-12-24 06:58:02 +0000

    update ngnix

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,32 @@
+#  chua su dung ngnix
+# #!/bin/bash
+
+# echo "--- Bắt đầu quy trình cập nhật tự động ---"
+
+# # 1. Kéo bản mới nhất từ Docker Hub về
+# sudo docker compose pull
+
+# # 2. Khởi động lại các container với image mới (không làm gián đoạn các dịch vụ khác)
+# sudo docker compose up -d
+
+# # 3. Dọn dẹp các image cũ bị thừa để tránh đầy ổ cứng
+# sudo docker image prune -f
+
+# echo "--- Cập nhật thành công vào lúc: $(date) ---"
+
+# su dung ngnix
 #!/bin/bash
+cd /home/nashies/hoc_devops/autodocker/hoc_compose/
 
-echo "--- Bắt đầu quy trình cập nhật tự động ---"
-
-# 1. Kéo bản mới nhất từ Docker Hub về
+echo "--- Dang keo code moi tu Docker Hub ---"
+# Pull tat ca cac image moi (ca user-api va nginx neu co)
 sudo docker compose pull
 
-# 2. Khởi động lại các container với image mới (không làm gián đoạn các dịch vụ khác)
-sudo docker compose up -d
+echo "--- Dang tai khoi dong he thong ---"
+# Up se tu dong tao moi container nao co ban cap nhat
+sudo docker compose up -d --remove-orphans
 
-# 3. Dọn dẹp các image cũ bị thừa để tránh đầy ổ cứng
+echo "--- Dang don dep image cu ---"
 sudo docker image prune -f
 
-echo "--- Cập nhật thành công vào lúc: $(date) ---"
+echo "Cap nhat thanh cong qua Nginx!"
