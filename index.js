04c100606ae023c9487e05074f13970ee399e471
notVinh <dqv4work@gmail.com>
2025-12-24 06:18:42 +0000

    update hook 2

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ app.get('/users/:email', async (req, res) => {
 });
 
 app.get('/', (req, res) => {
-    res.send("Chào mừng đến với dịch vụ người dùng!, DevOps is Awesome, Vinh2, , ifa!");
+    res.send("Vinh dep trai!");
 });
 
 app.listen(5000, () => console.log("Server đang chạy tại cổng 5000"));
